7611b21bcb8d345cf7f6bf4966b541010bbddee9
Isadora dias <117326440+dvisa7178@users.noreply.github.com>
2024-07-12 00:19:04 -0300

    implemantação do método

---
+++ rungekutta.m
@@ -0,0 +1,7 @@
+function Y = runge_kutta(x,y,h);
+    k_1 = funcrunge(x,y);
+    k_2 = funcrunge(x+0.5*h,y+0.5*h*k_1);
+    k_3 = funcrunge((x+0.5*h),(y+0.5*h*k_2));
+    k_4 = funcrunge((x+h),(y+k_3*h));
+    Y = y + (1/6)*(k_1+2*k_2+2*k_3+k_4)*h; 
+  
