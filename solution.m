da77f981cb9c04f380dae407e9273662d63beb9b
Isadora dias <117326440+dvisa7178@users.noreply.github.com>
2024-08-13 00:21:52 -0300

    Update solution.m

--- solution.m
+++ solution.m
@@ -1,9 +1,14 @@
 %% Dados do problema
 clear all; clc;
 
-h = 0.1;
+h = 0.001;
 t0 = 0.0;
-tf = 10.0;
+tf = 0.5;
+
+t = t0:h:tf;
+n = length(t); 
+
+
 
 %% Parâmetros 
 a1 = 1;      a2 = 1;
@@ -15,20 +20,31 @@ m_m1 = 5;    m_m2 = 5;
 I_m1 = 0.01; I_m2 = 0.01;
 g = 9.81;
 
+
 %% Início dos cálculos
 
+%olhar no outro código como calcular a posição (x,y) do ef
+
 % Condição inicial
-y0 = [0.1; 0.1; 0.0; 0.0];
-x = t0:h:tf;
-n = length(x);
-%y = zeros(4,n);
-y(:, 1) = y0;
+u = [0.0;1.0];
+y0 = [0.1; 0.1; 0.1; 0.1];
+gravidade = [100.0;0.0];
+%posição inicial é de (0.2,0)
+t = t0:h:tf;
+n = length(t); 
+Y = y0;
+y = y0;
 
 for i = 1:n-1
 
     % transformando thetas em y's
     theta1 = y(1, i);
     theta2 = y(2, i);
+    % aqui deve ser calulada a posição (da mesma forma em que foi feito no
+    % código anterior)
+    %plotar também o comportamento do robô
+
+
     theta1_ponto = y(3, i);
     theta2_ponto = y(4, i);
 
@@ -47,18 +63,21 @@ for i = 1:n-1
     g1 = (m_l1*l1 + m_m2*a1 + m_l2*a1)*g*cos(theta1) + m_l2*l2*g*cos(theta1+theta2);
     g2 = m_l2*l2*g*cos(theta1+theta2);
     
-    %Mi = matriz inercia (massas)
+    
     Mi = [b11, b12; b21, b22];
-    %C = ...
     C = [c11, c12; c21, c22];
-    %g = ...
-    g = [g1;g2];
-    %A = coisa completa 
+    g_vet = [g1;g2];
+    gravidade(1:2,i+1) = g_vet;
+    
+ 
     A = [zeros(2,2), eye(2,2); inv(Mi)*0, -inv(Mi)*C];
-    %B = outra coisa multiplicando os torques
     B = [zeros(2,2);inv(Mi)];
 
-    y(:, i+1) = rungekutta(x(i), y(:, i),h,A,B,g);
+    Y = rungekutta(t(i),Y,h,A,B,g_vet,u);
+    % x vira t arrumar depois
+    t(i+1) = t(i) + h;
+    y(1:4,i+1) = Y;
+
 end
 
 %% Plot dos yRunge-Kutta e da tabela de valores
@@ -70,13 +89,12 @@ end
 %uitable('Data',table_data,'ColumnName',{'y'},'Position',[140 100 400 300])
 %title('Valores de y')
 
-%figure;
-plot(x,y(1,:),'bo',x,y(2,:),'g',x,y(3,:),'r',x,y(4,:),'ko')
+figure;
+plot(t,y(1,:),'k',t,y(2,:),'g',t,y(3,:),'r',t,y(4,:),'b')
 title('Aproximações para Runge-Kutta')
+legend('y_1','y_2','y_3','y_4')
 xlabel('x')
 ylabel('f(x,y)')
 
-
-
-
-
+figure;
+plot(t,gravidade(1,:),'k',t,gravidade(2,:),'b')
