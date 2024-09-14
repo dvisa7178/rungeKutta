130619432084084429481e5b257039cc2f654e42
Isadora dias <117326440+dvisa7178@users.noreply.github.com>
2024-09-14 01:24:52 -0300

    Update funcrunge.m
    att funcrunge

--- funcrunge.m
+++ funcrunge.m
@@ -1,6 +1,6 @@
-function Y = funcrunge(x,y,A,B,g);
+function Y = funcrunge(x,y,A,B,g_vet,u);
 
      y = [y(1);y(2);y(3);y(4)];
-     u = [0.0;0.0];
-     Y = A*y - B*g + B*u;
+     u = [u(1);u(2)];
+     Y = A*y - B*g_vet + B*u;
 end
