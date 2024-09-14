a0980d56a92b924ea6a0c86d691640ac1db7c946
Isadora dias <117326440+dvisa7178@users.noreply.github.com>
2024-09-14 01:29:20 -0300

    Update rungekutta.m
    att rungekutta

--- rungekutta.m
+++ rungekutta.m
@@ -1,9 +1,9 @@
-function R = runge_kutta(x,y,h,A,B,g);
+function Y = runge_kutta(x,y,h,A,B,g_vet,u);
 %provavelmente u também
-    k_1 = funcrunge(x,y,A,B,g);
-    k_2 = funcrunge(x+0.5*h,y+0.5*h*k_1,A,B,g);
-    k_3 = funcrunge((x+0.5*h),(y+0.5*h*k_2),A,B,g);
-    k_4 = funcrunge((x+h),(y+k_3*h),A,B,g);
-    R = y + (1/6)*(k_1+2*k_2+2*k_3+k_4)*h; 
+    k_1 = funcrunge(x,y,A,B,g_vet,u);
+    k_2 = funcrunge(x+0.5*h,y+0.5*h*k_1,A,B,g_vet,u);
+    k_3 = funcrunge((x+0.5*h),(y+0.5*h*k_2),A,B,g_vet,u);
+    k_4 = funcrunge((x+h),(y+k_3*h),A,B,g_vet,u);
+    Y = y + (1/6)*(k_1+2*k_2+2*k_3+k_4)*h; 
 end
 
